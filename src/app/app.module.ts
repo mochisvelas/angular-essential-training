436ce4caaadf98560f80b8d225a332b1b46c6166
mochisvelas <velasquezmochis@gmail.com>
2021-02-05 23:43:01 -0600

    finish chapter 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MediaItemComponent } from './media-item.component';
 import { MediaItemListComponent } from './media-item-list.component';
-import { FavoriteDirective} from './favorite.directive'
+import { FavoriteDirective } from './favorite.directive'
+import { CategoryListPipe } from './category-list.pipe'
 
 @NgModule({
   imports: [
@@ -13,7 +14,8 @@ import { FavoriteDirective} from './favorite.directive'
     AppComponent,
     MediaItemComponent,
     MediaItemListComponent,
-    FavoriteDirective
+    FavoriteDirective,
+    CategoryListPipe
   ],
   bootstrap: [
     AppComponent
